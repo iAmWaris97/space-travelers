9483df8af52781e719eb6b8d3fe084a3499f5130
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-11 23:14:36 +0430

    Add routes to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import Header from './components/Navigation/Header';
+import Rockets from './components/Rockets/Rocket';
+import Missions from './components/Missions/Mission';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
