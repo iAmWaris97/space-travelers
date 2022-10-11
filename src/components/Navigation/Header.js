ceac9c4460f7f06779165998da83be89abeaeb63
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-11 22:10:02 +0430

    Refactore styles

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -2,13 +2,13 @@ import './Header.css';
 import Logo from './img/planet.png';
 
 const Header = () => (
-  <header className="header">
-    <div className="logo">
+  <header className="header flex">
+    <div className="logo flex">
       <img src={Logo} alt="logo" className="logo-img" />
       <h1 className="logo-title">Space Traveler&apos;s Hub</h1>
     </div>
     <nav className="navbar">
-      <ul className="nav-list">
+      <ul className="nav-list flex">
         <li>Rockets</li>
         <li>Missions</li>
         <li>My Profile</li>
