46150bc2e2fcb7754926c1f622c67a675a73d56d
mule <belmeetmule@gmail.com>
2022-10-12 01:54:15 +0300

    create a dummy list of missions and pass it to the missionlist component

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,4 +1,27 @@
+import MissionList from './MissionList';
+
+const missions = [
+  {
+    mission_id: '9D1B7E0',
+    mission_name: 'Thaicom',
+    description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+  },
+  {
+    mission_id: '9D1B7E1',
+    mission_name: 'Telstar',
+    description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+  },
+  {
+    mission_id: '9D1B7E2',
+    mission_name: 'Iridium NEXT',
+    description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+  },
+];
+
 const Missions = () => (
-  <h1>Missions</h1>
+  <>
+    <h1>Missions</h1>
+    <MissionList missionsProp={missions} />
+  </>
 );
 export default Missions;
