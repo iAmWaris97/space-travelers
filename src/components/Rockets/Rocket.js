1d97323cfacd8f13ff7bc3b2d70cfb6c22b7fbed
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-12 18:10:49 +0430

    Install redux and modify rocket component

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types';
+import Img from '../Navigation/img/planet.png';
 
 const Rocket = (props) => {
-  const { rocket } = props;
+  const { name, description } = props;
   return (
-    <div className="">
-      <h2>
-        {' '}
-        {rocket.rocket_name}
-      </h2>
+    <div className="rocket-card">
+      <div className="rocket-image">
+        <img src={Img} alt="rocket" className="rocket-img" />
+      </div>
+
+      <div className="rocket-detail">
+        <div className="rocket-name">
+          <span>{name}</span>
+        </div>
+        <div className="rocket-description">
+          <span>{description}</span>
+        </div>
+        <button type="button" className="reserve-btn">Reserve Rocket</button>
+      </div>
     </div>
   );
 };
+
 Rocket.propTypes = {
-  rocket: PropTypes.shape({
-    rocket_name: PropTypes.string,
-  }).isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
+
 export default Rocket;
