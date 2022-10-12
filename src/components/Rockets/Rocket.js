f0c0748ba3c839e4d6501d45d80d0147a6245c21
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-12 21:22:31 +0430

    Refactore rocket component

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,31 +1,21 @@
-import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
-import Img from './img/falcon1.jfif';
 
 const Rocket = (props) => {
-  const { name, description } = props;
+  const { rocket } = props;
   return (
-    <div className="rocket-card flex">
-      <div className="rocket-image">
-        <img src={Img} alt="rocket" className="rocket-img" />
-      </div>
-
-      <div className="rocket-detail">
-        <div className="rocket-name">
-          <span>{name}</span>
-        </div>
-        <div className="rocket-description">
-          <span>{description}</span>
-        </div>
-        <Button type="button" variant="primary" className="reserve-btn">Reserve Rocket</Button>
-      </div>
+    <div className="rocket">
+      <h2>
+        {' '}
+        {rocket.rocket_name}
+      </h2>
     </div>
   );
 };
 
 Rocket.propTypes = {
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  rocket: PropTypes.shape({
+    rocket_name: PropTypes.string,
+  }).isRequired,
 };
 
 export default Rocket;
