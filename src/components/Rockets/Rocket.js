faf202af1471cfe73950ea3cb32f23425087122f
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-13 03:17:08 +0430

    Implement reserve and cancel buttons

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../../redux/Rockets/rocketsSlice';
 
 const Rocket = (props) => {
+  const dispatch = useDispatch();
   const { rocket } = props;
   return (
     <div className="rocket-card flex">
@@ -13,8 +18,18 @@ const Rocket = (props) => {
           <span>{rocket.rocket_name}</span>
         </div>
         <div className="rocket-description">
-          <span>{rocket.description}</span>
+          <span>
+            {rocket.reserved ? <Badge bg="info">Reserved</Badge> : ''}
+            {rocket.description}
+          </span>
         </div>
+        <Button
+          variant={rocket.reserved ? 'outline-primary' : 'primary'}
+          size="lg"
+          onClick={() => dispatch(reserveRocket(rocket.rocket_id))}
+        >
+          {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+        </Button>
       </div>
     </div>
   );
@@ -22,9 +37,11 @@ const Rocket = (props) => {
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
+    rocket_id: PropTypes.string,
     rocket_name: PropTypes.string,
     description: PropTypes.string,
     flickr_images: PropTypes.arrayOf(PropTypes.string),
+    reserved: PropTypes.bool,
   }).isRequired,
 };
 
