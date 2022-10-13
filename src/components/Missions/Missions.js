5857517d882f27c34917ea6e0fcc5ba004fc8038
mule <belmeetmule@gmail.com>
2022-10-14 02:37:02 +0300

    fix linter erros

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -10,7 +10,7 @@ const Missions = () => {
     if (!missions.length) {
       dispatch(getMissions());
     }
-  }, []);
+  });
 
   const handleMissionJoin = (id) => {
     dispatch(joinMission(id));
