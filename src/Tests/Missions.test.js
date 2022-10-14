b0ba6dd6d8bf2183e9c18a0fe7e0a0d6bd0f0060
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-14 14:21:02 +0430

    Fix linter erros

--- src/Tests/Missions.test.js
+++ src/Tests/Missions.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Missions from '../components/Missions/missions';
+import Missions from '../components/Missions/Missions';
 import store from '../redux/configureStore';
 
 describe('Testing Missions', () => {
