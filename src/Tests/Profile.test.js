afa3cbf59ce03273f55c44a707aa97bfb47815a3
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-14 13:38:49 +0430

    Test profile

--- src/Tests/Profile.test.js
+++ src/Tests/Profile.test.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Missions from '../components/Missions/missions';
+import Profile from '../components/Profile/Profile';
 import store from '../redux/configureStore';
 
-describe('Testing Missions', () => {
-  it('Should render missions', () => {
+describe('Testing profile', () => {
+  it('Should render profile page', () => {
     const tree = render(
       <React.StrictMode>
         <Provider store={store}>
           <Router>
-            <Missions />
+            <Profile />
           </Router>
         </Provider>
       </React.StrictMode>,
