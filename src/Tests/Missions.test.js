7b7474855d0ea8a7077222971ba938f177a65018
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-14 13:30:28 +0430

    Test missions

---
+++ src/Tests/Missions.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Missions from '../components/missions/missions';
+import store from '../redux/configureStore';
+
+describe('Testing Missions', () => {
+  it('Should render missions', () => {
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Missions />
+          </Router>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
